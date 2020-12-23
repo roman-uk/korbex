e18e67da023f5bade3b34ee168fc33c20f287a56
roman-uk <roman55@mail.com>
2020-12-23 18:19:32 +0100

    add blog views

--- korbex/urls.py
+++ korbex/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from korbex import views
 
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('home', views.home, name='home_p'),
     path('store', views.store, name='store_p'),
     path('service', views.service, name='service_p'),
-    path('blog', views.blog, name='blog_p'),
+    path('blog/int:id', views.blog, name='blog_p'),
+    # path('blog', views.blog, name='blog_p'),
     path('contact', views.contact, name='contact_p'),
 ]
--- korbex/views.py
+++ korbex/views.py
@@ -2,29 +2,39 @@ from django.shortcuts import render
 from .models import *
 
 
+# Start
 def home(request):
     homecontent = HomeContent.objects.all()
-    blogcontent = Blog.objects.all()[:5]
+    blogcontent = Blog.objects.all().order_by("data_add")[:5]
     context = {"homecontent": homecontent, 'blogcontent': blogcontent}
     return render(request, 'korbex/home.html', context)
 
 
+# Sklep
 def store(request):
     context = {}
     return render(request, 'korbex/store.html')
 
 
+# Serwis
 def service(request):
     context = {}
     return render(request, 'korbex/service.html')
 
 
+# Blog
 def blog(request):
+    blog_id = request.GET.get("id", '')
+    if blog_id != '':
+        home_blog = Blog.objects.get(id=blog_id)
+    else:
+        home_blog = None
     blogcontent = Blog.objects.all().order_by("data_add")
-    context = {'blogcontent': blogcontent}
+    context = {'blogcontent': blogcontent, "home_blog": home_blog}
     return render(request, 'korbex/blog.html', context)
 
 
+# Kontact
 def contact(request):
     context = {}
     return render(request, 'korbex/contact.html')
